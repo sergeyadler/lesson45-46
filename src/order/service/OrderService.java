b6a661380bc69acfb3430f64a5e7d349095407c2
sergeyadler <sergeyadler28@gmail.com>
2025-06-23 12:31:59 +0200

    finished OrderServiceTest and OrderService

--- src/order/service/OrderService.java
+++ src/order/service/OrderService.java
@@ -10,11 +10,14 @@ public class OrderService {
     private final List<Order> orders = new ArrayList<>();
 
     public void addOrder(Order order) {
-        //TODO
+        if(order != null){
+            orders.add(order);
+        }
     }
 
     public List<Order> getAllOrders() {
-        return null; //TODO
+        return new ArrayList<>(orders);
+
     }
 
     public int getSize() {
@@ -22,6 +25,10 @@ public class OrderService {
     }
 
     public Order getOrderByName(String name) {
-        return null;
+        return orders.stream()
+                .filter(order -> order.getDish().equalsIgnoreCase(name))
+                .findFirst()
+                .orElse(null);
+
     }
 }
