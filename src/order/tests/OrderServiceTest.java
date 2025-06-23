653f51941e80d24a16876456e58a22edabd02233
sergeyadler <sergeyadler28@gmail.com>
2025-06-23 10:22:40 +0200

    generate OrderServiceTest

--- src/order/tests/OrderServiceTest.java
+++ src/order/tests/OrderServiceTest.java
@@ -1,5 +1,35 @@
 package order.tests;
 
-public class OrderServiceTest {
+import order.model.Order;
+import order.service.OrderService;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
+public class OrderServiceTest {
+    static private OrderService orderService;
+    private List<Order> expectedOrders;
+
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void addOrder() {
+    }
+
+    @Test
+    void getAllOrders() {
+    }
+
+    @Test
+    void getSize() {
+    }
+
+    @Test
+    void getOrderByName() {
+    }
 }
